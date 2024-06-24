580914a208b032c95a77c94082b58fd5b8fad932
FrankySuazo <frankysuazo@outlook.com>
2024-06-23 22:50:12 -0400

    js

--- index.js
+++ index.js
@@ -43,3 +43,9 @@ const getStandardDeviation = (array) => {
   const standardDeviation = Math.sqrt(variance);
   return standardDeviation;
 };
+
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
