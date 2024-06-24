ea5c9e0380ca084a8dfe4978c7da05198c458723
FrankySuazo <frankysuazo@outlook.com>
2024-06-24 13:10:44 -0400

    js

--- index.js
+++ index.js
@@ -48,4 +48,11 @@ const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+   
+    const mean = getMean(numbers);
+    const median = getMedian(numbers);
+    const mode = getMode(numbers);
+    const range = getRange(numbers);
+    const variance = getVariance(numbers);
+    const standardDeviation = getStandardDeviation(numbers);
   
